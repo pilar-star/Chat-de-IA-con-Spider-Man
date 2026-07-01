02bb92db02e4e5d08a78230b276f7dfcbba3473e
Pilar <pilarlugones0@gmail.com>
2026-07-01 17:07:46 -0300

    fix: se arreglan etiquetas semanticas de los <p> para que tengan clase y sean mas fáciles de estilizar

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import { initChat } from './chat.js';
 
 const routes = {
-    '/home': '<p1>BIENVENIDO</p1><p2>Esta página fue pensada y construida para hablar con el mejor héroe de la historia de los héroes, así que adelante:</p2><a href="/chat" id="iniciochat">¡Chatea con...Spider-Man!</a>',
+    '/home': '<p class="p1">BIENVENIDO</p1><p class="p2">Esta página fue pensada y construida para hablar con el mejor héroe de la historia de los héroes, así que adelante:</p2><a href="/chat" id="iniciochat">¡Chatea con...Spider-Man!</a>',
     '/chat': `
         <div class="chat-container">
             <div id="chat-messages" class="messages-area"></div>
@@ -10,7 +10,7 @@ const routes = {
                 <button type="submit">Enviar</button>
             </form>
         </div>`,
-    '/about': '<p1>Acerca de esta página</p1><p2>Aplicación creada para hablar tu amigable vecino...Spider-Man, también conocido como el Hombre Araña. Se eligió este personaje por su gran humor sarcastico, su buen corazon y su capacidad para tejer telarañas.</p2>'
+    '/about': '<p class="p1">Acerca de esta página</p1><p class="p2">Aplicación creada para hablar tu amigable vecino...Spider-Man, también conocido como el Hombre Araña. Se eligió este personaje por su gran humor sarcastico, su buen corazon y su capacidad para tejer telarañas.</p2>'
 };
 
 function navigateTo(url) {
@@ -20,7 +20,7 @@ function navigateTo(url) {
 
 function router() {
     const path = window.location.pathname === '/' ? '/home' : window.location.pathname;
-    const content = routes[path] || '<p1>Haz click en </p1><p2>"INICIO"</p2><p1>para recibir una bienvenida. Click en</p1><p2>"CHAT"</p2><p1>para comenzar a hablar con Spider-Man o click en</p1><p2>"ABOUT"</p2><p1>para saber más sobre la página</p1>';
+    const content = routes[path] || '<p class="p2">Haz click en </p2><p class="p1">INICIO</p1><p class="p2">para recibir una bienvenida. Click en</p2><p class="p1">CHAT</p1><p class="p2">para comenzar a hablar con Spider-Man. Click en</p2><p class="p1">ABOUT</p1><p class="p2">para saber más sobre la página</p2>';
     
     document.getElementById('app').innerHTML = content;
 
